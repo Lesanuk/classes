ff38e4ae967c204fe5e4722f50e01778ef966bcd
Lesie <lesyka4267@gmail.com>
2026-08-15 10:57:23 +0600

    Add files via upload

---
+++ duck_typing.py
@@ -0,0 +1,23 @@
+# "Duck Typing" = "If it looks like a duck and quacks like a duck then it must be a duck"
+
+class Animal:
+    alive = True
+
+class Dog(Animal):
+    def speak(self):
+        print("Woof!")
+
+class Cat(Animal):
+    def speak(self):
+        print("Meow!")
+
+class Car:
+    alive = True
+    def speak(self):
+        print("HONK!")
+
+animals = [Dog(), Cat(), Car()]
+
+for animal in animals:
+    animal.speak()
+    print(animal.alive)
